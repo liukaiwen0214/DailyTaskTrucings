ebf0e7293887c0448bffd85e294685210b91123c
liukaiwen0214 <lkwyouxiang@126.com>
2023-12-05 14:29:58 +0800

    上传测试

--- src/main/webapp/WEB-INF/AllJs/login.js
+++ src/main/webapp/WEB-INF/AllJs/login.js
@@ -1,1 +1,12 @@
-console.log("dimsid")
+//js 输出语句
+// window.alert()，写入弹出框
+// document.write()，写入html页面
+// console.log() 写入浏览器控制台
+// document.write("即将跳转到百度")
+// setTimeout(function () {
+//
+//     location.href = "https://www.baidu.com"
+// },3000)
+document.getElementById("testButton").onclick = function dianjial() {
+    alert("dianji")
+}
